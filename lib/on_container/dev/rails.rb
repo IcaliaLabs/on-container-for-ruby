a4bbca3555f028c353f2f8663c1d83ed874497c4
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-11-12 11:33:29 -0600

    Update

--- lib/on_container/dev/rails.rb
+++ lib/on_container/dev/rails.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 require 'on_container/dev/rails_ops'
+require 'on_container/dev/setup_ops'
 require 'on_container/dev/bundler_ops'
 require 'on_container/dev/node_modules_ops'
 require 'on_container/dev/active_record_ops'
 require 'on_container/dev/container_command_ops'
 
 include OnContainer::Dev::RailsOps
+include OnContainer::Dev::SetupOps
 include OnContainer::Dev::BundlerOps
 include OnContainer::Dev::NodeModulesOps
 include OnContainer::Dev::ActiveRecordOps
