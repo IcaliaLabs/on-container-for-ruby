e83c2bb29500d3b30e9772cea8dc308729a89da5
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-11-12 11:30:38 -0600

    Activerecord ops

--- lib/on_container/dev/rails.rb
+++ lib/on_container/dev/rails.rb
@@ -3,9 +3,11 @@
 require 'on_container/dev/rails_ops'
 require 'on_container/dev/bundler_ops'
 require 'on_container/dev/node_modules_ops'
+require 'on_container/dev/active_record_ops'
 require 'on_container/dev/container_command_ops'
 
 include OnContainer::Dev::RailsOps
 include OnContainer::Dev::BundlerOps
 include OnContainer::Dev::NodeModulesOps
+include OnContainer::Dev::ActiveRecordOps
 include OnContainer::Dev::ContainerCommandOps
