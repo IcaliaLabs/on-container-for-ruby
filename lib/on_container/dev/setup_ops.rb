90ea18b014962145cc17a9660540e740827a00ca
Roberto Quintanilla <1915931+vovimayhem@users.noreply.github.com>
2022-02-22 19:28:28 -0600

    Allow skipping setup lock (#26)
    * Updated Gemfile lock (???)
    
    * Skip container setup lock when 'ON_CONTAINER_SKIP_SETUP_LOCK' is true
    
    * Freeze the mf

--- lib/on_container/dev/setup_ops.rb
+++ lib/on_container/dev/setup_ops.rb
@@ -4,25 +4,37 @@ module OnContainer
   module Dev
     module SetupOps
       APP_PATH = File.expand_path '.'
-  
-      def app_temp_path; "#{APP_PATH}/tmp"; end
-      def app_setup_wait; ENV.fetch('APP_SETUP_WAIT', '5').to_i; end
-      def app_setup_lock_path; "#{app_temp_path}/setup.lock"; end
-  
+      TRUEISH_VALUES = %w[true True TRUE yes Yes YES].freeze
+      SKIP_SETUP_ENV_VAR = 'ON_CONTAINER_SKIP_SETUP_LOCK'.freeze
+
+      def app_temp_path
+        "#{APP_PATH}/tmp"
+      end
+
+      def app_setup_wait
+        ENV.fetch('APP_SETUP_WAIT', '5').to_i
+      end
+
+      def app_setup_lock_path
+        "#{app_temp_path}/setup.lock"
+      end
+
       def lock_setup
         system "mkdir -p #{app_temp_path} && touch #{app_setup_lock_path};"
       end
-      
+
       def unlock_setup
         system "rm -rf #{app_setup_lock_path}"
       end
-      
+
       def wait_setup
         puts 'Waiting for app setup to finish...'
         sleep app_setup_wait
       end
 
       def on_setup_lock_acquired
+        return yield if TRUEISH_VALUES.include? ENV[SKIP_SETUP_ENV_VAR]
+
         wait_setup while File.exist?(app_setup_lock_path)
 
         lock_setup
@@ -32,7 +44,6 @@ module OnContainer
         end
 
         yield
-
       ensure
         unlock_setup
       end
@@ -42,7 +53,7 @@ module OnContainer
           rails rspec sidekiq hutch puma rake webpack webpack-dev-server
         ].include?(ARGV[0])
       end
-      
+
       def command_might_require_database?
         %w[
           rails rspec sidekiq hutch puma rake
